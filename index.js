964f94f37399a69404ec1f2f1f9ba9b29fe170e9
Aras <114133924+Dirigentas@users.noreply.github.com>
2022-10-18 13:07:22 +0300

    numbers strings

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log( 1+2+'2')
+console.log(123);
+console.log('Labas rytas');
+console.log(2 + 2);
+console.log('2 + 2');
+console.log('2 + 2 =', 2 + 2);
+
+// Single line comment
+
+/*
+Multi
+line
+comment
+*/
+
+/*
+Esmines JS temos:
+- kintamieji - informacijos isiminimas
+- salygos sakiniai - palyginimas/patikrinimas
+- ciklai - pasikartojanciu proceduru kartojimas iki kol reikia
+- funkcijos/klases - pasikartojancios logikos surinkimas i viena vieta
+- event listeners - (front'o) stebejimas kada ir koki veiksma padare partotojas
+*/
